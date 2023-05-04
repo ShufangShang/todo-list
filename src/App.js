d4496a0859850ff6eb8d1044089a16c0f1fb55d9
Shufang Shang <shufang.x.shang@gmail.com>
2023-05-03 22:45:52 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+// State hook - 'useState'
+const [newItem, setNewItem] = useState("");
+const [items, setItems] = useState([]);
+
+// Helper Functions
+
+function addItem(){
+
+  if(!newItem){
+    alert("Enter an item.")
+    return;
+  }
+   
+  const item = {
+    id: Math.floor(Math.random()*1000),
+    value: newItem
+  };
+
+  setItems(oldList => [...oldList, item]);
+  setNewItem("");
+}
+
+function deleteItem(id){
+
+  const newArray = items.filter(item => item.id != id);
+  setItems(newArray);
+
+}
+
+  return (
+    <div className="App">
+      {/*1. Header */}
+      <h1>Todo List App</h1>
+
+      {/*2. Input (input and button)*/}
+      <input 
+      type="text"
+      placeholder="Add an item..,"
+      value={newItem}
+      onChange={e => setNewItem(e.target.value)}
+      />
+
+      <button onClick={() => addItem()}>Add</button>
+      
+
+      {/*3. List of Items (unordered list with list items)*/}
+
+      <ul>
+        {items.map(item => {
+          return(
+            <li key={item.id}>{item.value}<button className="delete-button" onClick={() => deleteItem(item.id)}>❌</button></li>
+          )
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
